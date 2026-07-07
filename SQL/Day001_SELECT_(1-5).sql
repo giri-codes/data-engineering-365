56cd7fe95b6bda8ce4d1052d9c1ac070b743ed17
giri-codes <222788569+giri-codes@users.noreply.github.com>
2026-07-07 12:30:02 +0530

    Complete Day001 SQL SELECT practice

---
+++ SQL/Day001_SELECT_(1-5).sql
@@ -0,0 +1,26 @@
+-- SQL Foundations
+-- SELECT
+
+-- Query 1: Select all columns from Employees table
+SELECT *
+FROM employees;
+
+
+-- Query 2: Select all column from Departments table
+SELECT *
+FROM departments;
+
+
+-- Query 3: Select all column from Projects table
+SELECT *
+FROM projects;
+
+
+-- Query 4: Select all columns from Customers table
+SELECT *
+FROM customers;
+
+
+-- Query 5: Select all columns from Orders table
+SELECT *
+FROM orders;
