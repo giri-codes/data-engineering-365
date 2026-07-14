02c235ceddc7be00ad8499868c19af3a412b6909
giri-codes <222788569+giri-codes@users.noreply.github.com>
2026-07-14 05:35:14 +0530

    Complete Day002 Python Variables Practice

--- Python/Day002_Variables_(6-10).py
+++ Python/Day002_Variables_(6-10).py
@@ -1,34 +1,34 @@
-# Day 02 - Variables (6-10)
+# Day 002 - Variables (6-10)
 
 # Program 6 - Store employees salary
-salary = 45000
-print("Employees salary:", salary)
+employee_salary = 45000
+print("Employees salary:", employee_salary)
 
 
 # Program 7 -Store customer count
-customer_count = 315
-print("Customer count :", customer_count)
+customer_count = 1456
+print("Total Customers:", customer_count)
 
 
 # Program 8 - Store monthly revenue
-monthly_revenue = 2500000
+monthly_revenue = 585645.75
 print("Monthly_revenue :", monthly_revenue)
 
 
 # Program 9 - Store ETL batch size
-etl_batch_size = 1000
+etl_batch_size = 1200
 print("ETL Batch Size :", etl_batch_size)
 
 
-# Program 10 - Mini Business Scenario - Store employee information using variables and display all details.
-employee_name = "Gireesh"
-employee_id = 101
-department = "IT"
-salary = 250000
-city = "Mysuru"
+# Program 10 - Mini Business Scenario - Store Company Information using variables and display all details.
+company_name = "EFG Technologies"
+employee_count = 545
+monthly_revenue = 1550000.25
+city = "Bengaluru"
+is_active = True
 
-print("Employee name :", employee_name)
-print("Employee ID   :", employee_id)
-print("Department    :", department)
-print("Salary        :", salary)
-print("City          :", city)
+print("Company name :", company_name)
+print("Employee Count :", employee_count)
+print("Monthly Revenue :", monthly_revenue)
+print("City :", city)
+print("Active Status :", is_active)
