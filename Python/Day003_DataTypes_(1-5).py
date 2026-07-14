9d1ba431ee7d4d8df30881282b65a2d1d06345f4
giri-codes <222788569+giri-codes@users.noreply.github.com>
2026-07-15 03:53:50 +0530

    Add Day003 Python Data Types Practice (1-5)

--- Python/Day003_DataTypes_(1-5).py
+++ Python/Day003_DataTypes_(1-5).py
@@ -1,32 +1,33 @@
-# Day 03 - Data Types (1-5)
+# Topic: Data Types 
+# Day003 - Variables(Programs 1–5)
 
-# Program 1 - Check Data Type of Integer
-num = 100
-print(type(num))
-
-
-# Program 2 - Check Data Type of Float
-salary = 50000.50
-print(type(salary))
-
-
-# Program 3 - Check Data Type of String
-name = "Giri"
-print(type(name))
-
-
-# Program 4 - Check Data Type of Boolean
-is_employee = True
-print(type(is_employee))
-
-
-# Program 5 - Display Multiple Data Types
+# Program 1 - Create an integer variable
 employee_id = 101
-employee_name = "Rahul"
-salary = 65000.75
-is_active = True
+print("Employee ID:", employee_id)
+
+
+# Program 2 - Create a float variable
+Product_price = 799.99
+print("Product Price:", Product_price)
+
+
+# Program 3 - Create a string variable
+employee_name = "Gireesh"
+print("Employee Name:", employee_name)
+
+
+# Program 4 - Create a Boolean variable
+is_employee_active = True
+print("Employee Active:", is_employee_active)
+
+
+# Program 5 - Check the data type of different variables using the type() function.
+employee_id = 102
+salary = 275000.50
+employee_name = "Gireesh"
+is_employee_active = True
 
 print(type(employee_id))
-print(type(employee_name))
 print(type(salary))
-print(type(is_active))
+print(type(employee_name))
+print(type(is_employee_active))
